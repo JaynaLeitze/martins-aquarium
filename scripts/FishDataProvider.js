e95dbc101287d9c2a6d22571bcfa0757a2e5929f
Jayna Leitze <jayna.leitze@gmail.com>
2020-10-07 21:59:25 -0500

    update data provider add functions

--- scripts/FishDataProvider.js
+++ scripts/FishDataProvider.js
@@ -8,7 +8,7 @@ const fishCollection = [
     {
             Name: "Dot",
             Species: "Sailfin Tang",
-            Size: "Up to 15 inches",
+            Length: "Up to 15 inches",
             Location: "South Pacific",
             Diet: "microalgae",
             Image: "https://i.redd.it/stdlqgg3b6811.jpg",
@@ -16,7 +16,7 @@ const fishCollection = [
     {
         Name: "Fabio",
         Species: "Orange Spotted Filefish",
-        Size: 9,
+        Length: 9,
         Location: "Great Barrier Reef",
         Diet: "Carnivore",
         Image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQXX7Wa1YHylF9Vl5PjyUTRzD0jkm-NqlOZeg&usqp=CAU",
--- scripts/FishList.js
+++ scripts/FishList.js
@@ -1,17 +1,25 @@
+import { Fish } from "./fish.js"
 /**
  *  FishList which renders individual fish objects as HTML
  */
+import {useFish} from "./FishDataProvider.js"
 
-// TODO: Import `useFish` from the data provider module
 export const FishList = () => {
-const contentElement =document.querySelector(".contentContainer__left")
+const contentElement = document.querySelector(".contentContainer__left")
+
+const fishes = useFish()
+
+let fishHTMLRepresentations = ""
+for (const fish of fishes){
+    fishHTMLRepresentations += Fish(fish)
+}
 
 contentElement.innerHTML +=`
-<section class="FishList">
-<h3>Aquarium Fish</h3>
-<div class="fishContainer">
-    Placeholder
-    </div> 
-    </section>
-    `
+<section class="fishList">
+    <h3>Aquarium Fish</h3>
+    <div class="fishContainer">
+    ${fishHTMLRepresentations}
+    </div>
+</section>
+`
 }
